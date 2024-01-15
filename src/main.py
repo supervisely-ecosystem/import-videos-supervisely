cdcc8af6e6e5e6e8bfdeea49d3f9658b04343711
cxnt <pavelbartsits@gmail.com>
2024-01-15 15:53:08 +0400

    add file import support

--- src/main.py
+++ src/main.py
@@ -14,9 +14,15 @@ def import_videos(api: sly.Api, task_id: int):
     if len(dir_info) == 0:
         raise Exception(f"There are no files in selected directory: '{g.INPUT_PATH}'")
 
+    sly.logger.debug(f"Number of files in selected directory: {len(dir_info)}")
+
     project = None
     if g.IMPORT_MODE in ["project", "dataset"] and g.PROJECT_ID is None:
-        project_name = f.get_project_name_from_input_path(g.INPUT_PATH)
+        project_name = (
+            f.get_project_name_from_input_path(g.INPUT_PATH)
+            if len(g.OUTPUT_PROJECT_NAME) == 0
+            else g.OUTPUT_PROJECT_NAME
+        )
         sly.logger.warning("Existing project wasn`t selected. Creating new project...")
         project = api.project.create(
             workspace_id=g.WORKSPACE_ID,
@@ -24,7 +30,9 @@ def import_videos(api: sly.Api, task_id: int):
             change_name_if_conflict=True,
             type=sly.ProjectType.VIDEOS,
         )
-        sly.logger.info(f'New project has been created - "{project.name}" (ID: {project.id})')
+        sly.logger.info(
+            f'New project has been created - "{project.name}" (ID: {project.id})'
+        )
     elif g.IMPORT_MODE in ["project", "dataset"]:
         project = api.project.get_info_by_id(id=g.PROJECT_ID)
         if project is None:
@@ -41,7 +49,9 @@ def import_videos(api: sly.Api, task_id: int):
             change_name_if_conflict=True,
             type=sly.ProjectType.VIDEOS,
         )
-        sly.logger.info(f'New project has been created - "{project.name}" (ID: {project.id})')
+        sly.logger.info(
+            f'New project has been created - "{project.name}" (ID: {project.id})'
+        )
     project_name = project.name
 
     datasets_names, datasets_videos_map = f.get_datasets_videos_map(dir_info)
@@ -49,7 +59,9 @@ def import_videos(api: sly.Api, task_id: int):
     for dataset_name in datasets_names:
         dataset_info = None
         if g.IMPORT_MODE == "dataset" and g.DATASET_NAME is not None:
-            dataset_info = api.dataset.get_info_by_name(parent_id=project.id, name=g.DATASET_NAME)
+            dataset_info = api.dataset.get_info_by_name(
+                parent_id=project.id, name=g.DATASET_NAME
+            )
         if dataset_info is None:
             dataset_info = api.dataset.create(
                 project_id=project.id, name=dataset_name, change_name_if_conflict=True
@@ -63,7 +75,9 @@ def import_videos(api: sly.Api, task_id: int):
         result_video_paths = []
         result_video_names = []
 
-        convert_progress = sly.Progress(message="Converting videos", total_cnt=len(videos_names))
+        convert_progress = sly.Progress(
+            message="Converting videos", total_cnt=len(videos_names)
+        )
         for video_name, video_path, video_size in progress_bar(
             zip(videos_names, videos_paths, videos_sizes),
             total=len(videos_paths),
@@ -83,7 +97,6 @@ def import_videos(api: sly.Api, task_id: int):
         for batch_video_paths, batch_video_names in zip(
             sly.batched(result_video_paths), sly.batched(result_video_names)
         ):
-
             upload_progress = []
 
             def _print_progress(monitor, upload_progress):
@@ -108,9 +121,13 @@ def import_videos(api: sly.Api, task_id: int):
     if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
         api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_PATH)
         source_dir_name = g.INPUT_PATH.lstrip("/").rstrip("/")
-        sly.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
+        sly.logger.info(
+            msg=f"Source directory: '{source_dir_name}' was successfully removed."
+        )
 
-    api.task.set_output_project(task_id=task_id, project_id=project.id, project_name=project.name)
+    api.task.set_output_project(
+        task_id=task_id, project_id=project.id, project_name=project.name
+    )
 
 
 @sly.handle_exceptions(has_ui=False)
@@ -124,6 +141,7 @@ def main():
         },
     )
     import_videos(g.api, g.TASK_ID)
+
 
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
